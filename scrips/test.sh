03ba49c83318fd59d90ee058f263b46675721d67
Davidezorz <davide.zorzetto01@gmail.com>
2025-09-08 15:23:14 +0200

    test.sh

--- scrips/test.sh
+++ scrips/test.sh
@@ -8,6 +8,9 @@
 #SBATCH -t 00:09:00                 # 5 minutes for profiling and test runs
 #SBATCH --job-name=testing
 
+T=8
+P=1
+
 # Set OpenMP variables
 export OMP_NUM_THREADS=8
 export OMP_PLACES=cores
@@ -19,6 +22,6 @@ module load openMPI/5.0.5
 
 mpicc -D_XOPEN_SOURCE=700 -O3 -std=c17 -fopenmp -march=native  -Iinclude   src/stencil_template_parallel.c   -o main
 # mpirun -np 1 ./stencil_template_parallel -p 1 -v 0 -o 0 > output_base.log
-srun --ntasks=1 --cpus-per-task=8 ./main -o 0 -e 300 -v 1 > output_base.log
+srun --ntasks=1 --cpus-per-task=8 ./main -o 0 -e 300 -v 1 > output_T${T}P${P}.log
 
 

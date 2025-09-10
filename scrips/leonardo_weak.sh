9b61c7fdc3f04555fe36ba20d6075fc1f179637a
Davide Zorzetto <dzorzett@login07.leonardo.local>
2025-09-11 01:17:27 +0200

    weak leonardo

--- scrips/leonardo_weak.sh
+++ scrips/leonardo_weak.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --nodes=1                   # number of nodes
+#SBATCH --nodes=8                   # number of nodes
 #SBATCH --cpus-per-task=8           # OpenMP threads per MPI task
 #SBATCH --ntasks-per-node=8         # how many task each node
 
@@ -7,7 +7,7 @@
 #SBATCH --partition dcgp_usr_prod
 #SBATCH -A uTS25_Tornator_0
 
-#SBATCH -t 00:00:30
+#SBATCH -t 00:02230
 #SBATCH --job-name=HPC_Exam
 #SBATCH --exclusive
 # =======================================================
@@ -15,12 +15,12 @@
 # Load MPI module if needed
 module load openmpi/4.1.6--gcc--12.2.0
 
-N=1                                 # nodes       -> nodes
-P=8                                 # processors  -> number of task
+N=8                                 # nodes       -> nodes
+P=64                                 # processors  -> number of task
 T=8                                 # threads     -> CPU per task
 
-x=$((16384))
-y=$((16384))
+x=$((16384*4))
+y=$((16384*2))
 
 
 # Set OpenMP variables
@@ -51,7 +51,7 @@ mpicc -D_XOPEN_SOURCE=700 -o main -march=native -O3 -std=c17 -fopenmp -Iinclude 
     
     
 start_time=$(date +%s.%N)
-srun --ntasks=$P --cpus-per-task=$T --cpu-bind=cores  ./main -x 32768 -y 32768 -p 1 -o 0 -e 300 -v 0 > leonardo_N${N}P${P}T${T}.log
+srun --ntasks=$P --cpus-per-task=$T --cpu-bind=cores  ./main -x $x -y $y -p 1 -o 0 -e 300 -v 0 > leonardo_N${N}P${P}T${T}x${x}y${y}.log
 end_time=$(date +%s.%N)
 tot_time=$(echo "$end_time - $start_time" | bc)
 echo "TimeTotalFile: ${tot_time}" >> leonardo_N${N}P${P}T${T}.log
